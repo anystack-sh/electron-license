fb2250cb76c5374804b98462901024f3e5c376b8
Philo Hermans <me@philohermans.com>
2021-07-29 15:02:35 +0200

    wip

--- src/app.js
+++ src/app.js
@@ -1,81 +1,129 @@
 import Alpine from 'alpinejs';
 
-
 const { ipcRenderer } = require('electron');
 const axios = require('axios');
-
-
 
 window.Unlock = () => {    
 
     return {
+        isReady: false,
+
         prompt: {
             title: 'Unlock',
             subtitle: 'Activate your license to get started',
-            logo: 'https://unlock.sh/img/unlock-logo-grey.svg'
+            logo: 'https://unlock.sh/img/unlock-logo-grey.svg',
+            email: 'Email address',
+            licenseKey: 'License key',
+            activateLicense: 'Activate license',
+            errors: {
+                'NOT_FOUND': 'Your license information did not match our records.',
+                'SUSPENDED': 'Your license has been suspended.',
+                'EXPIRED': 'Your license has been expired.',
+                'FINGERPRINT_MISSING': 'Device fingerprint is missing.',
+                'FINGERPRINT_ALREADY_EXISTS': 'An active license already exist for this device.',
+                'MAX_USAGE_REACHED': 'Your license has reached it\'s activation limit.',
+            }
         },
         
         confirmation: {
             title: 'License activated',
             subtitle: 'Thank you for your support',
-            action: 'Open Application'
         },
 
-        productId: null,
-        apiKey: null,
+        api: {
+            productId: null,
+            key: null,
+            url: 'https://api.unlock.sh/v1',
+        },
+
+        license: {
+            fingerprint: false,
+            requireEmail: false,
+        },
 
         loading: false,
+
         email: 'john@snow.com',
+        emailError: null,
         licenseKey: '8d562d27-43d4-4032-b988-4f697f17e487',
-        error: null,
+        licenseError: null,
 
         activated: false,
         activation: {},
 
         init() {
             const params = new URLSearchParams(global.location.search);
-    
-            var data =  JSON.parse(params.get('data'))        
+            const data = JSON.parse(params.get('data'));
 
-            this.title = data.title;
-            this.subtitle = data.subtitle;
+            Object.assign(this.prompt, data.prompt ?? {});
+            Object.assign(this.confirmation, data.confirmation ?? {});
+            Object.assign(this.api, data.api ?? {});
+            Object.assign(this.license, data.license ?? {});
+
             this.logo = data.logo;
-            this.productId = data.productId;
-            this.apiKey = data.apiKey;
+
+            if(this.api.fingerprint === true) {
+                ipcRenderer.send('get-device-fingerprint');
+                ipcRenderer.on('set-device-fingerprint', (event, arg) => {
+                    this.api.fingerprint = arg;
+                });
+            }
+
+            setTimeout(() => {
+                this.isReady = true;
+            }, 1000);
         },
 
         activateLicense() {
             this.loading = true;
             this.error = null;
 
-            axios.post(`http://api.unlock.test/v1/products/${this.productId}/licenses/activate-key`, {
+            let data = {
                 key: this.licenseKey
-            }, 
+            };
+
+            if(this.license.requireEmail) {
+                data = Object.assign(data, {
+                    scope: {
+                        licensee: {
+                            email: this.email,
+                        }
+                    }
+                })
+            }
+
+            if(this.api.fingerprint) {
+                data.fingerprint = this.api.fingerprint;
+            }
+
+            axios.post(`${this.api.url}/products/${this.api.productId}/licenses/activate-key`, data,
             {
                 headers: {
-                    'Authorization': `Bearer ${this.apiKey}`
+                    'Authorization': `Bearer ${this.api.key}`
                 }
             })
             .then((response) => {        
                 if(response.status === 201) {
-                    this.activation = response.data;
-                    this.activated = true;
+                    setTimeout(() => {
+                        this.activation = response.data;
+                        this.activated = true;
+
+                        setTimeout(() => {
+                            ipcRenderer.sendSync('license-activated', { licenseKey: this.licenseKey, email: this.email, fingerprint: this.activation.fingerprint});
+                        }, 3000);
+                    }, 500);
                 }              
             })
             .catch((error) => {
                 this.loading = false;
                 if(error.response.status === 422) {
-                    this.error = error.response.data.meta.message;
+                    this.licenseError = this.prompt.errors[error.response.data.errors['license']] ?? null;
+                    this.emailError = error.response.data.errors['scope.licensee.email'] ?? null;
                 }
             })
             .then(() => {
                 
             });
-        },
-
-        openApplication() {
-            
-            ipcRenderer.sendSync('license-activated');
         }
     }
 
--- src/index.js
+++ src/index.js
@@ -1,34 +1,104 @@
 'use strict';
 
-const { BrowserWindow } = require('electron');
-// import {machineIdSync} from 'node-machine-id';
+const {BrowserWindow, ipcMain} = require('electron');
 const {machineIdSync} = require('node-machine-id');
+const Store = require('electron-store');
+const dayjs = require('dayjs');
+const axios = require('axios');
 
 module.exports = class Unlock {
-  constructor(config) {
-      this.config = config;
-  }
+    constructor(config) {
+        this.config = config;
+        this.store = new Store({ name: 'unlock', encryptionKey: this.config.api.productId });
 
-  hasActiveLicense() {
-
-      // Create the browser window.
-      const mainWindow = new BrowserWindow({
-        width: 400,
-        height: 500,
-        alwaysOnTop: true,
-        resizable: false,
-        minimizable: false,
-        titleBarStyle: 'hidden',
-        webPreferences: {
-          nodeIntegration: true,
-          contextIsolation: false,
+        if (this.config.fingerprint) {
+            this.handleFingerprint()
         }
-      });
+    }
 
-      // and load the index.html of the app.
-      mainWindow.loadFile(`${__dirname}/../../node_modules/unlock-electron-license/src/app.html`, {query: {"data": JSON.stringify(this.config)}});
+    hasActiveLicense() {
+        if (this.store.has('license')) {
 
-      // Open the DevTools.
-      mainWindow.webContents.openDevTools();
-  }
+            this.hasValidLicense();
+            if(this.checkinRequired()) {
+            }
+
+            return true;
+        }
+
+        this.promptLicenseWindow();
+    }
+
+    promptLicenseWindow() {
+        // Create the browser window.
+        const licenseWindow = new BrowserWindow({
+            width: 400,
+            height: 450,
+            resizable: false,
+            frame: false,
+            webPreferences: {
+                nodeIntegration: true,
+                contextIsolation: false,
+            }
+        });
+
+        // and load the index.html of the app.
+        licenseWindow.loadFile(`${__dirname}/../../node_modules/unlock-electron-license/src/app.html`, {query: {"data": JSON.stringify(this.config)}});
+
+        // Open the DevTools.
+        // licenseWindow.webContents.openDevTools();
+
+        ipcMain.on('license-activated', (event, arg) => {
+            this.store.set('license', {
+                key: arg.licenseKey,
+                email: arg.email,
+                fingerprint: arg.fingerprint,
+                lastCheckIn: dayjs().unix()
+            });
+
+            licenseWindow.close();
+        });
+    }
+
+    checkinRequired() {
+        const lastCheckIn = this.store.get('license.lastCheckIn');
+
+        return dayjs().subtract(24, 'hours').isAfter(dayjs.unix(lastCheckIn));
+    }
+
+    hasValidLicense() {
+        const licenseKey = this.store.get('license.key');
+        const fingerprint = this.store.get('license.fingerprint');
+        const data = {
+            key: licenseKey,
+            fingerprint: fingerprint
+        };
+
+        axios.post(`${this.config.api.url}/products/${this.config.api.productId}/licenses/validate-key`, data,
+            {
+                headers: {
+                    'Authorization': `Bearer ${this.config.api.key}`
+                }
+            })
+            .then((response) => {
+                const valid = response.data.meta.valid;
+
+                if(valid === false) {
+                    this.store.delete('license');
+                }
+
+                console.log('success', response);
+            })
+            .catch((error) => {
+                console.log('fail', error);
+            })
+            .then(() => {
+            });
+    }
+
+    handleFingerprint() {
+        ipcMain.on('get-device-fingerprint', (event, arg) => {
+            event.reply('set-device-fingerprint', machineIdSync())
+        })
+    }
 }
