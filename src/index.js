88ceec714eda007ff1df990eddf1c09875ba5d16
Andreas Frøyland <81354124+Andreas-Froyland@users.noreply.github.com>
2023-11-17 14:23:43 +0100

    add missing slash

--- src/index.js
+++ src/index.js
@@ -149,7 +149,7 @@ module.exports = class Unlock {
                 prepend += '/';
             }
 
-            this.licenseWindow.loadFile(path.resolve(__dirname, prepend + 'license/license.html'), { query: { "data": JSON.stringify(this.config) } });
+            this.licenseWindow.loadFile(path.resolve(__dirname, prepend + '/license/license.html'), { query: { "data": JSON.stringify(this.config) } });
         } else {
             this.licenseWindow.loadFile(process.resourcesPath + '/license/license.html', { query: { "data": JSON.stringify(this.config) } });
         }
